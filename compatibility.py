bd452d451bb177544be3f9e21f51af45f6e4da05
nocem <55653371+nocem@users.noreply.github.com>
2019-10-03 11:29:27 +0200

    Update compatibility.py

--- compatibility.py
+++ compatibility.py
@@ -2,28 +2,38 @@
 
 from octoprint.util.commandline import CommandLineCaller
 import octoprint.plugin
-import logging
 
 caller = CommandLineCaller()
+fwtype = 0 #undefined
 
 class Plugin(octoprint.plugin.EventHandlerPlugin):
-  on_event(event, payload):
+  def on_event(event, payload):
     if event == "Connected":
       caller.call(["M115"])
   
-  gcode_rec(comm_instance, line, *args, **kwargs):
+  def gcode_rec(comm_instance, line, *args, **kwargs):
     if "ZWLF make it" is in line:
       fwtype = 1 #ChiTu
-      return line
-    if 
+    
+    if fwtype == 1 && line.startswith("wait"):
+      return "echo:busy processing"
+    
+    return line
+  
+  def gcode_que(comm_instance, phase, cmd, cmd_type, gcode, subcode=None, tags=None, *args, **kwargs):
+    if fwtype == 1 && gcode == "M110":
+      return "M110"
+    
+    return
       
       
 
 
 __plugin_name__ = "fwfix"
 __plugin_author__ = "nocem"
-__plugin_version__ = "0.0.1"
+__plugin_version__ = "0.1.0"
 __plugin_hooks__ = {
-  "octoprint.comm.protocol.gcode.received": gcode_rec
+  "octoprint.comm.protocol.gcode.received": gcode_rec,
+  "octoprint.comm.protocol.gcode.queing": gcode_que
 }
 __plugin_implementation__ = Plugin()
