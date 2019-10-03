55e3a7be71801f0b73fff47e7c6a2ae83cd8fafd
nocem <55653371+nocem@users.noreply.github.com>
2019-10-03 09:28:49 +0200

    Update temp_remap.py

--- temp_remap.py
+++ temp_remap.py
@@ -4,6 +4,9 @@ def remap_temp(comm_instance, parsed_temperatures, *args, **kwargs):
    return 12.3
 
 __plugin_name__ = "Temperature remap"
+__plugin_author__ = "nocem"
+__plugin_version__ = "1.0"
+__plugin_url__ = "https://github.com/nocem/main-/3d-printing/temp_remap.py"
 __plugin_hooks__ = {
    "octoprint.comm.protocol.temperatures.received": remap_temp
 }
